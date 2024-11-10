7d6e3d3ba0059b5f5ca22e7615f467a91eb9509c
baqwas <baqwas@gmail.com>
2024-11-10 16:26:16 -0600

    thread_predict.py works

---
+++ predict/thread_predict.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""
+@brief thread_predict.py run a thread-safe inference  with a pretrained YOLO11 model
+@version 0.1
+@date 2024-10-20
+@author armw
+
+@brief This script will infer objects in the input source using YOLO11
+
+Copyright (C) 2024 ParkCircus Productions; All Rights Reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Usage:
+thread_predict.py
+
+script adapted from
+@sa https://docs.ultralytics.com/modes/predict/#thread-safe-inference
+
+@sa https://docs.ultralytics.com/modes/predict/#why-use-ultralytics-yolo-for-inference
+@sa https://docs.voxel51.com/integrations/ultralytics.html
+@sa https://github.com/amikelive/coco-labels/blob/master/coco-labels-2014_2017.txt
+
+Adapted from: https://docs.ultralytics.com/tasks/detect/#predict
+"""
+import os
+from threading import Thread
+from ultralytics import YOLO
+
+def thread_safe_predict(image_path): #  helper function
+    """
+    Performs thread-safe inference on an image using a locally instantiated YOLO model
+    :param image_path:  image for inference
+    :return:
+    """
+    local_model = YOLO("yolo11n.pt")
+    results = local_model.predict(image_path)
+
+    for result in results:
+        result.show()  # display to screen
+
+Thread(target=thread_safe_predict, args=("/home/reza/PycharmProjects/yolo11/images/parrots.jpg",)).start()
+Thread(target=thread_safe_predict, args=("/home/reza/PycharmProjects/yolo11/images/birds.jpg",)).start()
+
